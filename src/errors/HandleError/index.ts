fba065c5d1fbe600c7898c98c1166e2842c89b46
Theus <matheusth.dev@gmail.com>
2023-11-24 11:13:02 -0300

    feat: handle error middleware

---
+++ src/errors/HandleError/index.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../AppError";
+
+export const handleErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: "error", code: err.statusCode, message: err.message });
+  } else {
+    return res
+      .status(500)
+      .json({ status: "error", code: 500, message: "internal server error" });
+  }
+};
