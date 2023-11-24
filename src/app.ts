fe803cd4a322a69a92deb4aefb5b11492b9e0465
Theus <matheusth.dev@gmail.com>
2023-11-24 11:22:55 -0300

    feat: error middleware application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "express-async-errors";
+import { handleErrorMiddleware } from "./errors/HandleError";
+import { AppError } from "./errors/AppError";
 
 export const app = express();
 
@@ -8,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("", (req: Request, res: Response) => {
+  // throw new AppError(404, "Teste de erro não encontrado");
   return res.json({ message: "Teste técnico da empresa Escribo" });
 });
+
+app.use(handleErrorMiddleware);
