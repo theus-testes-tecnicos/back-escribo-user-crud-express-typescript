844692c845a9a13a4ce37d61ab5d2ceb79589b42
Theus <matheusth.dev@gmail.com>
2023-11-25 15:28:28 -0300

    feat: verify fieelds middleware

--- src/middleware/verifyCreateUserFields.middleware.ts
+++ src/middleware/verifyCreateUserFields.middleware.ts
@@ -1,3 +1,29 @@
 import { Request, Response, NextFunction } from "express";
+import { createUserSchema } from "../schemas/createUser.schema";
+import { AppError } from "../errors/AppError";
 
-export const verifyCreateUserFieldsMiddleware = async () => {};
+export const verifyCreateUserFieldsMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { nome, email, senha, telefones } = req.body;
+
+  if (!nome || !email || !senha || !telefones) {
+    throw new AppError(
+      400,
+      'The "nome", "email", "senha" and "telefones" fields are mandatory'
+    );
+  }
+
+  try {
+    const validated = createUserSchema.parse({ nome, email, senha, telefones });
+
+    console.log(validated);
+
+    next();
+  } catch (error) {
+    console.log("erro!");
+    console.log(error);
+  }
+};
