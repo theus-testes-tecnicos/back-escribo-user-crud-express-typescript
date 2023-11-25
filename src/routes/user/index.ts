74722970974132d20489e6f2e23161d74c7c9db6
Theus <matheusth.dev@gmail.com>
2023-11-25 15:30:36 -0300

    feat: create user route

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { isAuthenticatedMiddleware } from "../../middleware/isAuthenticated.middleware";
 import { getUsersController } from "../../controllers/user/getUsers.controller";
+import { createUserController } from "../../controllers/user/createUser.controller";
+import { verifyCreateUserFieldsMiddleware } from "../../middleware/verifyCreateUserFields.middleware";
 
 const router = Router();
 
 export const userRoutes = () => {
+  router.post("", verifyCreateUserFieldsMiddleware, createUserController);
   router.get("", isAuthenticatedMiddleware, getUsersController);
 
   return router;
