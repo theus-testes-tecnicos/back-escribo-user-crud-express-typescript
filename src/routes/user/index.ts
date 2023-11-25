99ca844ef6da9d906fb15ca3187884132c8f36ab
Theus <matheusth.dev@gmail.com>
2023-11-25 19:09:09 -0300

    feat: retrieve user route

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -3,12 +3,17 @@ import { isAuthenticatedMiddleware } from "../../middleware/isAuthenticated.midd
 import { getUsersController } from "../../controllers/user/getUsers.controller";
 import { createUserController } from "../../controllers/user/createUser.controller";
 import { verifyCreateUserFieldsMiddleware } from "../../middleware/verifyCreateUserFields.middleware";
+import { retrieveUserController } from "../../controllers/user/retrieveUser.controller";
 
 const router = Router();
 
 export const userRoutes = () => {
   router.post("", verifyCreateUserFieldsMiddleware, createUserController);
-  router.get("", isAuthenticatedMiddleware, getUsersController);
+
+  router.use(isAuthenticatedMiddleware);
+
+  router.get("", getUsersController);
+  router.get("/profile", retrieveUserController);
 
   return router;
 };
